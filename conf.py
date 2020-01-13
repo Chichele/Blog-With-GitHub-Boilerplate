92d9d5b80da24748a8e92ad1731c65b8acc23298
lzchao3 <lzchao3@grgbanking.com>
2020-01-13 22:32:59 +0800

    配置域名；更新头像

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ enable_jsdelivr = {
 
 # 站点设置
 site_name = "Don't ask me why"
-site_logo = "${static_prefix}logo.png"
+site_logo = "${static_prefix}logo.jfif"
 site_build_date = "2019-12-18T16:51+08:00"
 author = "Chichele"
 email = ""
